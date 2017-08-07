21ff848d437338450679c365fd0aba6a464e5208
bhucsapu <bhucsapu@BHUW5007.europe.ad.flextronics.com>
2017-08-07 16:03:53 +0200

    Copy Fixed with Javaxt

--- src/Backup_main.java
+++ src/Backup_main.java
@@ -5,101 +5,172 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+
 public class Backup_main {
-	
+
 	public static void main(String[] args) {
 		String user = "bhucsapu";
 		int num = 842;
 		int nul = 0;
-		File DirLocation = new File("C:/users/" + user);
+		javaxt.io.Directory input = new javaxt.io.Directory("C:/Users/" + user);
+		javaxt.io.Directory output = new javaxt.io.Directory("D:/BHUM" + nul + num);
+		String fileFilter = "*.json";
 		if (num < 1000) {
 			File theDir = new File("D:\\BHUM" + nul + num);
-			if (!theDir.exists()) {	
-			    System.out.println("creating directory: " + theDir.getName());
-			    boolean result = false;
-			    try{
-			        theDir.mkdir();
-			        result = true;
-			    } 
-			    catch(SecurityException se){
-			        //handle it
-			    }        
-			    if(result) {    
-			        System.out.println("DIR created");
-			        try{
-			        	copyFolder(DirLocation,theDir);
-			           }catch(IOException e){
-			        	e.printStackTrace();
-			        	//error, just exit
-			                System.exit(0);
-			           }
-			    }
-		}else {
-			System.out.println("Dir already Created");
-		}
-		}else {
+			if (!theDir.exists()) {
+				System.out.println("creating directory: " + theDir.getName());
+				boolean result = false;
+				try {
+					theDir.mkdir();
+					result = true;
+				} catch (SecurityException se) {
+					// handle it
+				}
+				if (result) {
+					System.out.println("DIR created");
+					
+					input.copyTo(output, true); // true to overwrite any existing files
+					// Spawn threads
+					int numThreads = 4;
+					java.util.ArrayList<Thread> threads = new java.util.ArrayList<Thread>();
+					for (int i = 0; i < numThreads; i++) {
+						Thread thread = new Thread(new FileCopier(input, output));
+						threads.add(thread);
+						thread.start();
+					}
+
+					// Initiate search
+					int numFiles = 0;
+					java.util.List results = input.getChildren(true, fileFilter, false);
+					while (true) {
+						Object item;
+						synchronized (results) {
+							while (results.isEmpty()) {
+								try {
+									results.wait();
+								} catch (InterruptedException e) {
+									break;
+								}
+							}
+							item = results.remove(0);
+							results.notifyAll();
+						}
+
+						if (item != null) {
+
+							if (item instanceof javaxt.io.File) {
+
+								// Add file to the file copier
+								javaxt.io.File file = (javaxt.io.File) item;
+								FileCopier.add(file);
+								numFiles++;
+							}
+						} else { // item is null. This is our queue that the search is done!
+							break;
+						}
+					}
+
+					// Notify threads that we are done adding files to the queue
+					FileCopier.done();
+					// Wait for threads to complete
+					while (true) {
+						try {
+							for (Thread thread : threads) {
+								thread.join();
+							}
+							break;
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+
+					threads.clear();
+
+					System.out.println("Copy complete!");
+
+				}
+			} else {
+				System.out.println("Dir already Created");
+			}
+		} else {
 			File theDir = new File("D:\\BHUM" + num);
-			if (!theDir.exists()) {	
-			    System.out.println("creating directory: " + theDir.getName());
-			    boolean result = false;			    
-			    try{
-			        theDir.mkdir();
-			        result = true;
-			    } 
-			    catch(SecurityException se){
-			        //handle it
-			    }        
-			    if(result) {    
-			        System.out.println("DIR created");  
-			    }
-		}else {
-			System.out.println("Dir already Created");
+			if (!theDir.exists()) {
+				System.out.println("creating directory: " + theDir.getName());
+				boolean result = false;
+				try {
+					theDir.mkdir();
+					result = true;
+				} catch (SecurityException se) {
+					// handle it
+				}
+				if (result) {
+					System.out.println("DIR created");
+				}
+			} else {
+				System.out.println("Dir already Created");
+			}
 		}
+
+		int numThreads = 4;
+		java.util.ArrayList<Thread> threads = new java.util.ArrayList<Thread>();
+		for (int i = 0; i < numThreads; i++) {
+			Thread thread = new Thread(new FileCopier(input, output));
+			threads.add(thread);
+			thread.start();
 		}
+
+		// Initiate search
+		int numFiles = 0;
+		java.util.List results = input.getChildren(true, fileFilter, false);
+		while (true) {
+			Object item;
+			synchronized (results) {
+				while (results.isEmpty()) {
+					try {
+						results.wait();
+					} catch (InterruptedException e) {
+						break;
+					}
+				}
+				item = results.remove(0);
+				results.notifyAll();
+			}
+
+			if (item != null) {
+
+				if (item instanceof javaxt.io.File) {
+
+					// Add file to the file copier
+					javaxt.io.File file = (javaxt.io.File) item;
+					FileCopier.add(file);
+					numFiles++;
+				}
+			} else { // item is null. This is our queue that the search is done!
+				break;
+			}
+		}
+
+		// Notify threads that we are done adding files to the queue
+		FileCopier.done();
+		// Wait for threads to complete
+		while (true) {
+			try {
+				for (Thread thread : threads) {
+					thread.join();
+				}
+				break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		threads.clear();
+
+		System.out.println("Copy complete!");
+
 	}
-	public static void copyFolder(File src, File dest)
-	    	throws IOException{
 
-	    	if(src.isDirectory()){
 
-	    		//if directory not exists, create it
-	    		if(!dest.exists()){
-	    		   dest.mkdir();
-	    		   System.out.println("Directory copied from "
-	                              + src + "  to " + dest);
-	    		}
-
-	    		//list all the directory contents
-	    		String files[] = src.list();
-
-	    		for (String file : files) {
-	    		   //construct the src and dest file structure
-	    		   File srcFile = new File(src, file);
-	    		   File destFile = new File(dest, file);
-	    		   //recursive copy
-	    		   copyFolder(srcFile,destFile);
-	    		}
-
-	    	}else{
-	    		//if file, then copy it
-	    		//Use bytes stream to support all file types
-	    			InputStream in = new FileInputStream(src);
-	    	        OutputStream out = new FileOutputStream(dest);
-
-	    	        byte[] buffer = new byte[1024];
-
-	    	        int length;
-	    	        //copy the file content in bytes
-	    	        while ((length = in.read(buffer)) > 0){
-	    	    	   out.write(buffer, 0, length);
-	    	        }
-
-	    	        in.close();
-	    	        out.close();
-	    	        System.out.println("File copied from " + src + " to " + dest);
-	    	}
-	    }
-	}
-
-		    
-		
+}
---
+++ src/FileCopier.java
@@ -0,0 +1,56 @@
+public class FileCopier implements Runnable {
+ 
+      private static java.util.List pool = new java.util.LinkedList();
+      private javaxt.io.Directory output;
+      private int pathLength = -1;
+ 
+ 
+      public FileCopier(javaxt.io.Directory input, javaxt.io.Directory output) {
+          pathLength = input.getPath().length();
+          this.output = output;
+      }
+ 
+      public static void add(javaxt.io.File file) {
+          synchronized (pool) {
+             pool.add(pool.size(), file);
+             pool.notify(); //pool.notifyAll();
+          }
+      }
+ 
+      public static void done() {
+          synchronized (pool) {
+             pool.add(pool.size(), null);
+             pool.notify(); //pool.notifyAll();
+          }
+      }
+ 
+      public void run() {
+ 
+          while (true) {
+ 
+              javaxt.io.File file = null;
+              synchronized (pool) {
+                  while (pool.isEmpty()) {
+                      try {
+                          pool.wait();
+                      }
+                      catch (InterruptedException e) {
+                          return;
+                      }
+                  }
+                  file = (javaxt.io.File) pool.get(0);
+                  if (file!=null) pool.remove(0);
+                  pool.notifyAll();
+              }
+ 
+              if (file!=null){
+                  String relPath = file.getParentDirectory().getPath().substring(pathLength);
+                  javaxt.io.File outputFile = new javaxt.io.File(output + relPath + file.getName());
+                  file.copyTo(outputFile, true);
+              }
+              else{
+                  return;
+              }
+          }
+      }
+  }
